b6884b44a224da784d734f932e3f3e2b5915d285
Abhishek <negiabhishek883@gmail.com>
2022-05-25 20:57:50 +0530

    added new change to code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,23 @@
 package Java8Programs;
 
+import java.util.Scanner;
+
 public class HelloWorld 
 {
 	public static void main(String[] args) 
-	{
-	 System.out.println("Hello World");		
+	{		
+		
+	 Scanner sc = new Scanner(System.in);	
+	 System.out.println("Are you hungry?");
+	 String input = sc.nextLine();
+	 
+	 if(input.equalsIgnoreCase("yes"))
+	 {
+	   System.out.println("eat samosa");
+	 }
+	 else
+	 {
+		System.out.println("do your homework");
+	 }
 	}
 }
