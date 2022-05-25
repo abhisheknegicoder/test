72dfe8156087a27e414052eae3c9cf7487813c08
Abhishek <negiabhishek883@gmail.com>
2022-05-25 21:11:26 +0530

    new class added in code

---
+++ GitProgramChanges.java
@@ -0,0 +1,23 @@
+package Java8Programs;
+
+import java.util.Scanner;
+
+public class GitProgramChanges 
+{
+	public static void main(String[] args) 
+	{
+	 Scanner sc = new Scanner(System.in);	
+	 int number = sc.nextInt();
+	 
+	 if(number%2==0)
+	 {
+	   System.out.println("even number");
+	 }
+	 else
+	 {
+		System.out.println("odd number");
+	 }
+		
+	}
+
+}
